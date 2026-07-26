51455d5f1cd9e62ccefb96446839cc9cfd5b88c9
wolejnik <olejnik.wojciech1@gmail.com>
2026-07-26 21:20:25 +0200

    STAGE-3: Implemented global exception filters

--- stage-3/project/src/main.ts
+++ stage-3/project/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AllExceptionsFilter } from './common/exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,7 @@ async function bootstrap() {
       transform: true,
     }
   ));
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   const config = new DocumentBuilder()
     .setTitle('Products API')
